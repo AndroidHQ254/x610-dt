4c069c7c3d2131ef6d7fe54423a0274a31e343f9
Weilun Du <wdu@google.com>
2020-08-21 14:45:14 -0700

    x610: Add TetheringOverlay
    BUG: 163800715
    Change-Id: Ia0834f5215ff8c3b13effa591bd2f84d93419a06
    Signed-off-by: Weilun Du <wdu@google.com>

--- device.mk
+++ device.mk
@@ -178,6 +178,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/configstore@1.1.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/configstore@1.1.policy \
     $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 
+# Tethering
+PRODUCT_PACKAGES += \
+    TetheringConfigOverlay
+
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
