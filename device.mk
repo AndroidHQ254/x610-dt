51b980a96cbc2d8f97b78d4c65c15e80cf5e2952
Jaroslav Zviezda <acroreiser@gmail.com>
2022-04-02 21:08:24 +0100

    x610: Enable DM file pre-opting to reduce first boot time
    Signed-off-by: SakthivelNadar <s2234nadar@gmail.com>

--- device.mk
+++ device.mk
@@ -49,6 +49,9 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
     android.hardware.drm@1.4-service.clearkey
 
+# Enable DM file pre-opting to reduce first boot time
+PRODUCT_DEX_PREOPT_GENERATE_DM_FILES := true
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-service \
