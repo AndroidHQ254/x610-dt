1240ad207a9a8f43ddca2fb8370d8ac40e6e47dc
hpnightowl <iamhp2k@gmail.com>
2022-08-20 11:37:32 +0530

    x610: Drop audio.a2dp.default
    * build conflicts

--- device.mk
+++ device.mk
@@ -8,7 +8,6 @@
 PRODUCT_PACKAGES += \
     android.hardware.audio@4.0.vendor \
     android.hardware.soundtrigger@2.1-impl \
-    audio.a2dp.default \
     audio.bluetooth.default \
     audio.r_submix.default \
     audio.usb.default \
