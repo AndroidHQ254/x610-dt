99f99ac2f6e969231db2fe2032e27acb1158ade4
Hadenix <vsegda_online_007@bk.ru>
2023-07-30 21:29:30 +0300

    x610: Cleanup rootdir scripts

--- device.mk
+++ device.mk
@@ -161,21 +161,11 @@ PRODUCT_PACKAGES += \
 
 # Ramdisk
 PRODUCT_PACKAGES += \
-    factory_init.connectivity.rc \
-    factory_init.project.rc \
-    factory_init.rc \
-    init.ago.rc \
     init.connectivity.rc \
     init.modem.rc \
     init.mt6765.rc \
     init.mt6765.usb.rc \
-    init.project.rc \
     init.sensor_1_0.rc \
-    meta_init.connectivity.rc \
-    meta_init.modem.rc \
-    meta_init.project.rc \
-    meta_init.rc \
-    multi_init.rc \
     fstab.mt6765 \
     ueventd.mt6765.rc
 
