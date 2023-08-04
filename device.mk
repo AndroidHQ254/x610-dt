4a1faeeac7aac173d9e99e2860a872971894fa5d
AndroidHQ254 <d.murimi14@gmail.com>
2023-08-04 14:38:36 +0300

    x610: Address Imsinit derp

--- device.mk
+++ device.mk
@@ -88,7 +88,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml \
-    $(LOCAL_PATH)/configs/permissions/privapp-permissions-org.lineageos.umidigi.imsinit.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-org.lineageos.umidigi.imsinit.xml
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-org.lineageos.infinix.imsinit.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-org.lineageos.infinix.imsinit.xml
 
 # Keylayout
 PRODUCT_COPY_FILES += \
