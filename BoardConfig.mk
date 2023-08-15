d2a5988db3e6caea557b331974776ade755b0910
AndroidHQ254 <d.murimi14@gmail.com>
2023-08-16 02:03:03 +0300

    [TMP] x610: sepolicy: Address tons of denials

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -115,6 +115,7 @@ VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # VNDK
